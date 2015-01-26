ad742a351e5fa8706b4c95180c9e54ec7f75588b
jack <son.jack0218@gmail.com>
2015-01-26 18:37:17 -0500

    associated posts and cats

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :category_posts
-  has_many :categories through: :category_posts
+  has_many :categories, through: :category_posts 
 end
