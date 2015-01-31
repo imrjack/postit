9a68e94946fd666dbfe33dae101044e3a9c6dd4d
Jack <son.jack0218@gmail.com>
2015-01-31 17:58:25 -0500

    gemfile fix

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
-  source 'https://rubygems.org'
-  ruby '2.1.5'
+source 'https://rubygems.org'
 
 gem 'rails', '4.0.0'
 gem 'sass-rails', '~> 4.0.0'
--- db/migrate/20150126222911_destroytables.rb
+++
@@ -1,6 +0,0 @@
-class Destroytables < ActiveRecord::Migration
-  def change
-    drop_table :categories
-    drop_table :categories_posts
-  end
-end
